2ac908b6da8c2f0afdc06f9f3c7cc03d5a7d79bf
ashtiger1999 <aa26164095@gmail.com>
2025-04-28 17:09:40 +0900

    first commit

---
+++ Team4Semi-Project/src/main/java/kh/eclass/team4/project/Team4SemiProjectApplication.java
@@ -0,0 +1,13 @@
+package kh.eclass.team4.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4SemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4SemiProjectApplication.class, args);
+	}
+
+}
---
+++ Team4Semi-Project/src/test/java/kh/eclass/team4/project/Team4SemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.eclass.team4.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4SemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

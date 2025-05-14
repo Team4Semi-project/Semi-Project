27f2963a13b61783f6ea469288d61a1f5beab495
miae094 <miae094@gmail.com>
2025-05-14 17:42:43 +0900

    first commit

--- Team4Semi-Project/src/main/java/kr/co/lemona/member/model/dto/Member.java
+++ Team4Semi-Project/src/main/java/kr/co/lemona/member/model/dto/Member.java
@@ -12,13 +12,14 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Member {
 
- private int memberNo;            // 회원번호(PK)
- private String memberId;         // 회원 아이디
- private String memberName;       // 회원 이름
- private String memberNickname;   // 회원 닉네임
- private String memberEmail;      // 회원 이메일
- private String memberPw;         // 회원 비밀번호
- private String profileImg;       // 프로필 이미지 경로
- private String enrollDate;       // 가입일
- private String memberDelFl;      // 회원탈퇴여부 ('N' 또는 'Y')
+	private int memberNo; // 회원번호(PK)
+	private String memberId; // 회원 아이디
+	private String memberName; // 회원 이름
+	private String memberNickname; // 회원 닉네임
+	private String memberEmail; // 회원 이메일
+	private String memberPw; // 회원 비밀번호
+	private String profileImg; // 프로필 이미지 경로
+	private String enrollDate; // 가입일
+	private String memberDelFl; // 회원탈퇴여부 ('N' 또는 'Y')
+
 }

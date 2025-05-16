a0d86d75c75cbf3a85592d98f59d32e08786d43b
miae094 <miae094@gmail.com>
2025-05-16 14:20:53 +0900

    레시피 글 목록

--- Team4Semi-Project/src/main/java/kr/co/lemona/recipeBoard/controller/RecipeBoardController.java
+++ Team4Semi-Project/src/main/java/kr/co/lemona/recipeBoard/controller/RecipeBoardController.java
@@ -25,6 +25,14 @@ public class RecipeBoardController {
 	private RecipeBoardService service;
 	
   
+	/** 레시피 게시판 조회하여 리스트 가져오는 메서드
+	 * @param categoryNo : 레시피 게시판 카테고리 번호, 전체 누를경우 0이 기본값
+	 * @param cp : 현재 페이지
+	 * @param model : 서버에서 나온 결과값 화면단으로 전달
+	 * @param paramMap : 검색기능 개발 시 사용 예정
+	 * @return
+	 * @author miae
+	 */
 	@GetMapping("")
 	public String selectRecipeBoardList(@RequestParam(value = "categoryNo", defaultValue = "0") int categoryNo,
 								@RequestParam(value="cp", required = false, defaultValue = "1") int cp,
@@ -41,7 +49,7 @@ public class RecipeBoardController {
 			
 		} else { // 검색인 경우 --> paramMap
 			 			
-			// 검색 서비스 호출
+			// 검색 서비스 호출, 추후 개발 예정
 			// map = service.serchList(paramMap, cp);
 			
 		}		

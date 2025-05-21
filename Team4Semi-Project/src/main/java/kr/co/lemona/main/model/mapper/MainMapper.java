de4831924acef2a1cf6f8042ba777bf999329902
jihyun-00 <rkdwl811@naver.com>
2025-05-21 15:01:56 +0900

    검색기능

--- Team4Semi-Project/src/main/java/kr/co/lemona/main/model/mapper/MainMapper.java
+++ Team4Semi-Project/src/main/java/kr/co/lemona/main/model/mapper/MainMapper.java
@@ -6,15 +6,11 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.session.RowBounds;
 
+import kr.co.lemona.board.model.dto.Board;
 import kr.co.lemona.recipeBoard.model.dto.RecipeBoard;
 
 @Mapper
 public interface MainMapper {
-
-	/** 메인페이지 레시피 최신 게시글 조회
-	 * @return
-	 */
-	List<RecipeBoard> mainRecipeBoardList(RowBounds rowBounds);
 
 	/** 삭제되지 않은 게시글 갯수 조회
 	 * @return
@@ -34,6 +30,13 @@ public interface MainMapper {
 	int getPopularListCount();
 
 	List<RecipeBoard> selectPopularBoardList(RowBounds rowBounds);
+
+	/** 게시판 통합 검색 조회 결과
+	 * @param paramMap
+	 * @param rowBounds
+	 * @return
+	 */
+	List<Board> searchAllBoardList(Map<String, Object> paramMap, RowBounds rowBounds);
 
 	
 

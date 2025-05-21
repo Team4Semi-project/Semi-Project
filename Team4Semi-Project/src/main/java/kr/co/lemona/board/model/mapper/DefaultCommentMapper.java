dcfb04a82de7c83547c9fbb7538237c38d53f1aa
ITJANG <whalswkd1213@gmail.com>
2025-05-21 18:35:02 +0900

    defaultBoardDetail : comment.js에 넘겨줄 값 설정, comment 관련 : 댓글 기능 추가

--- Team4Semi-Project/src/main/java/kr/co/lemona/board/model/mapper/DefaultCommentMapper.java
+++ Team4Semi-Project/src/main/java/kr/co/lemona/board/model/mapper/DefaultCommentMapper.java
@@ -1,33 +1,36 @@
-package kr.co.lemona.board.model.service;
+package kr.co.lemona.board.model.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.co.lemona.board.model.dto.DefaultComment;
 
-public interface DefaultCommentService {
+@Mapper
+public interface DefaultCommentMapper {
 
-	/** 댓글목록 조회 서비스
+	/** 댓글 목록 조회
 	 * @param boardNo
 	 * @return
 	 * @author 민장
 	 */
 	List<DefaultComment> select(int boardNo);
 
-	/** 댓글/답글 등록 서비스
+	/** 댓글/답글 등록
 	 * @param comment
 	 * @return
 	 * @author 민장
 	 */
 	int insert(DefaultComment comment);
 
-	/** 댓글/답글 삭제 서비스
+	/** 댓글/답글 삭제
 	 * @param commentNo
 	 * @return
 	 * @author 민장
 	 */
 	int delete(int commentNo);
 
-	/** 댓글 수정 서비스
+	/** 댓글 수정
 	 * @param comment
 	 * @return
 	 * @author 민장

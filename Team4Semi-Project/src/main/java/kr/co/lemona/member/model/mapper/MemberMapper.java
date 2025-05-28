c431e5af9e5d5ef1cb6d9a147d87b7e848b4ab72
shinmyoungha <audgk0309@naver.com>
2025-05-28 17:43:37 +0900

    이메일 인증완료

--- Team4Semi-Project/src/main/java/kr/co/lemona/member/model/mapper/MemberMapper.java
+++ Team4Semi-Project/src/main/java/kr/co/lemona/member/model/mapper/MemberMapper.java
@@ -11,35 +11,31 @@ import kr.co.lemona.member.model.dto.Member;
 public interface MemberMapper {
    
 
-	/**
-	 * 로그인 SQL 실행
+	/** 로그인 SQL 실행
 	 * @param memberEmail
 	 * @return
 	 */
 	Member login(String memberId);
 
-	/**
-	 * 이메일 중복검사
+	/** 이메일 중복검사
 	 * @param memberEmail
 	 * @return
 	 */
 	int checkEmail(String memberEmail);
 
-	/**
-	 * 닉네임 중복검사
+	/** 닉네임 중복검사
 	 * @param memberNickname
 	 * @return
 	 */
 	int checkNickname(String memberNickname);
 	
-	/** 아이디 체크
+	/** 아이디 중복 체크
 	 * @param memberId
 	 * @return
 	 */
-	int checkId(String memberId); // 추가
+	int checkId(String memberId);
 
-	/**
-	 * 회원 가입
+	/** 회원 가입
 	 * @param inputMember
 	 * @return
 	 */
@@ -59,14 +55,11 @@ public interface MemberMapper {
 	Map<String, String> findUserByIdNameEmail(Member member);
 
 
-	/** 비밀번호 업데이트
-	 * @param memberId
-	 * @param newPassword
+	/** 비밀번호 재설정
+	 * @param params
 	 * @return
 	 */
-	boolean updatePassword(@Param("memberId") String memberId, @Param("newPassword") String newPassword);
-	
-
+	int updatePassword(Map<String, Object> params);
 
 
 }
